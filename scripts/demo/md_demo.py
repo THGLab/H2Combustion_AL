4a3efc641173a4308f474558eb5f2ebefb536681
Nancy <nguan@smith.edu>
2023-08-12 12:33:47 -0400

    adding comments

--- scripts/demo/md_demo.py
+++ scripts/demo/md_demo.py
@@ -1,5 +1,11 @@
 from md.md_ts_traj import run_md_simulation
-
+import timeit
+"""
+The file is a short demo that runs a 10fs molecular dynamics simulation using the active learning model.
+It takes in a starting structure (Transition state structure of rxn10), run the simulation with the 
+default active learning model(models/model_al_1kperrxn_1) and store outputs in demo/output.
+"""
+start = timeit.default_timer()
 
 data_dir = "../../md/irc_geom"
 rxn = '10'
@@ -7,5 +13,7 @@ start_structure = f"{data_dir}/TS_{rxn}.xyz"
 output_dir = 'demo_output'
 run_md_simulation(start_structure,output_dir,temp = 300, time_fs= 10, stepsize=0.1, calc_option="ML_QBC")
 
+end = timeit.default_timer()
+print(f"Time: {end-start} s")
 
 
